e9321edf97efa36c514ce181c26f7a3f561c9f97
SirHertzAlot <dukjoseph@gmail.com>
2024-05-27 05:16:35 -0400

    Finished index and started working on edit and search functionality for MVP. Search needs work and edit is almost done. Just need to work out bugs. -search doesn't want to detect full string from sub string and edit isn't showing link on html page.

--- entry/urls.py
+++ entry/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from . import views
+from .views import SearchResultsView
 
 app_name = "entries"
 
@@ -8,5 +9,7 @@ urlpatterns = [
     path("create/", views.index, name="create"),
     path("post/", views.submit_entry, name="post"),
     path("/<str:title>/", views.read_entry, name="read"),
-    path("random/", views.random_entry, name="random")
+    path("/random/", views.random_entry, name="random"),
+    path("/update/", views.edit_entry, name="edit"),
+    path("/search/<str:title>/", views.subQuery, name="search_results")
 ]
--- entry/views.py
+++ entry/views.py
@@ -4,8 +4,9 @@ from django import forms
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 from .models import Entry
+from django.db.models import Q
 
 from encyclopedia import util
 
@@ -15,9 +16,58 @@ class NewEntryForm(forms.Form):
 
 class EntryView(TemplateView):
     model = Entry
-    template_name = 'post_list.html'
+    template_name = 'encyclopedia/entry.html'
+
+class SearchResultsView(ListView):
+    model = Entry
+    template_name = 'encyclopedia/search_results.html'
+
+    def get_query(self):  # new
+        return res
 
 # Create your views here.
+
+#Sub query entry
+
+def subQuery(request):
+    query = request.GET.get("q")
+    query_list = util.list_entries()
+    res = [i for i in query_list if query in i]
+    entry = util.get_entry(res)
+    if entry is not None:
+        print(f"Received request from {request}")
+        return render(request, "encyclopedia/entry.html", {
+            "entry": entry
+        })
+    else:
+        print(f"Received request from {request}")
+        query_list = util.list_entries()
+        return render(request, "encyclopedia/search_results.html", {
+            "entries": entry
+        } )
+    
+#Edit entry route
+def edit_entry(request):
+    if request.method == "POST":
+        
+        form = NewEntryForm(request.POST)
+
+        # Check if form data is valid (server-side)
+        if form.is_valid():
+
+            # Isolate the entry from the 'cleaned' version of form data
+            title = form.cleaned_data["title"]
+            content = form.cleaned_data["content"]
+
+            # Add the new entry to our list of entries
+            util.save_entry(title, content)
+
+            # Redirect user to list of entries
+            return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "encyclopedia/edit.html", {
+            "form": NewEntryForm()
+        })
 
 # Create Entry view with form to send markdown entry to backend for validation & processing. 
 def index(request):
@@ -29,10 +79,10 @@ def index(request):
 def random_entry(request):
     
     if request.method == "GET":
-        entries = util.list_entries
+        entries = util.list_entries()
         entry = random.choice(entries)
         return render(request,"encyclopedia/read.html", {
-            "entry": entry
+            "entry": util.get_entry(entry)
         })
     else:
       # Redirect user to list of entries
